f43ac7a98cd9e012d9a6603314716738622c1af0
Smarshall <99678760+Smarshal21@users.noreply.github.com>
2023-04-26 03:27:36 +0530

    final

--- app/src/main/java/com/example/burnproject/MainActivity.kt
+++ app/src/main/java/com/example/burnproject/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.burnproject
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
@@ -17,6 +16,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
@@ -30,64 +30,125 @@ import android.content.Intent as Intent1
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var select_image_button : Button
-   // lateinit var img_view1 : ImageView
-    lateinit var img_view2 : ImageView
-    lateinit var camerabtn : Button
-    lateinit var predictbtn : Button
-    var uri:String = ""
-    var bitmap:Bitmap? = null
-    @SuppressLint("WrongThread")
+    lateinit var select_image_button: Button
+
+    // lateinit var img_view1 : ImageView
+    lateinit var img_view2: ImageView
+    lateinit var camerabtn: Button
+    lateinit var predictbtn: Button
+    lateinit var predictbtn2: Button
+    var uri: String = ""
+    var bitmap: Bitmap? = null
+    var encodedImage: String?=null
+    lateinit var resultee: TextView
+
     @RequiresApi(Build.VERSION_CODES.M)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent1?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 250) {
+            img_view2.setImageURI(data?.data)
+
+            var uuri: Uri? = data?.data
+            uri = uuri.toString()
+            bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uuri)
+            val imageBytes = runBlocking {
+                val baos = ByteArrayOutputStream()
+                bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+                baos.toByteArray()
+            }
+            encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT)
+
+
+        } else if (requestCode == 200 && resultCode == Activity.RESULT_OK) {
+            bitmap = data?.extras?.get("data") as Bitmap
+            img_view2.setImageBitmap(bitmap)
+        }
+        fun ARGBBitmap(img: Bitmap): Bitmap {
+            return img.copy(Bitmap.Config.ARGB_8888, true)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         select_image_button = findViewById(R.id.selectbtn)
-   //     img_view1 = findViewById(R.id.imageview1)
+        //     img_view1 = findViewById(R.id.imageview1)
         img_view2 = findViewById(R.id.imageview2)
         camerabtn = findViewById<Button>(R.id.capturebtn)
         predictbtn = findViewById<Button>(R.id.predictbtn)
+        resultee = findViewById(R.id.result)
+        predictbtn2 = findViewById<Button>(R.id.getbtn)
         checkandGetpermissions()
-        val baos = ByteArrayOutputStream()
-        bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-        val imageBytes = baos.toByteArray()
-        val encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT)
+
 
 
         select_image_button.setOnClickListener(View.OnClickListener {
             Log.d("mssg", "button pressed")
-            var intent : Intent1 = Intent1(Intent1.ACTION_GET_CONTENT)
+            var intent: Intent1 = Intent1(Intent1.ACTION_GET_CONTENT)
             intent.type = "image/*"
 
             startActivityForResult(intent, 250)
         })
 
         camerabtn.setOnClickListener(View.OnClickListener {
-            var camera : Intent1 = Intent1(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+            var camera: Intent1 = Intent1(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(camera, 200)
         })
         predictbtn.setOnClickListener {
             val retrofit = Retrofit.Builder()
-                .baseUrl("https://jsonplaceholder.typicode.com/posts/")
+                .baseUrl("https://2b22-2401-4900-5ac4-fd3e-8cf4-1df1-7b73-800a.ngrok-free.app/predict/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(
                     OkHttpClient.Builder()
                         .addInterceptor(HttpLoggingInterceptor().apply {
                             level = HttpLoggingInterceptor.Level.BODY
                         })
-                        .build())
+                        .build()
+                )
                 .build()
-
             val myApi = retrofit.create(MyApi::class.java)
 
-            val imageRequest = ImageRequest(encodedImage)
-            myApi.uploadImage(imageRequest).enqueue(object : Callback<ApiResponse> {
-                override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
-                    Toast.makeText(this@MainActivity, "Image posted", Toast.LENGTH_SHORT).show()
-                }
+            val imageRequest = encodedImage?.let { it1 -> ImageRequest(it1) }
+            if (imageRequest != null) {
+                myApi.uploadImage(imageRequest).enqueue(object : Callback<ApiResponse> {
+                    override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
+//                        val result = response.body()
+//                        resultee.text = result.toString()
 
-                override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+                        Toast.makeText(this@MainActivity, "Image posted", Toast.LENGTH_SHORT).show()
+
+                    }
+
+                    override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+                        Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
+                    }
+                })
+
+
+            }
+        }
+        predictbtn2.setOnClickListener {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://2b22-2401-4900-5ac4-fd3e-8cf4-1df1-7b73-800a.ngrok-free.app/predict/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(
+                    OkHttpClient.Builder()
+                        .addInterceptor(HttpLoggingInterceptor().apply {
+                            level = HttpLoggingInterceptor.Level.BODY
+                        })
+                        .build()
+                )
+                .build()
+            val myApi = retrofit.create(MyApi::class.java)
+            myApi.getFloat().enqueue(object : Callback<Float> {
+
+                override fun onResponse(call: Call<Float>, response: Response<Float>) {
+                    var result = response.body()
+                    resultee.text = result.toString()
+                }
+                override fun onFailure(call: Call<Float>, t: Throwable) {
                     Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
                 }
             })
@@ -96,52 +157,27 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray){
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == 100){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
-            {
+        if (requestCode == 100) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show()
-            }
-            else{
+            } else {
                 Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
             }
         }
     }
-    @RequiresApi(Build.VERSION_CODES.M)
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent1?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if(requestCode == 250){
-            img_view2.setImageURI(data?.data)
-
-            var uuri : Uri ?= data?.data
-            uri = uuri.toString()
-            bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uuri)
-        }
-        else if(requestCode == 200 && resultCode == Activity.RESULT_OK){
-            bitmap = data?.extras?.get("data") as Bitmap
-            img_view2.setImageBitmap(bitmap)
-        }
-        fun ARGBBitmap(img: Bitmap): Bitmap {
-            return img.copy(Bitmap.Config.ARGB_8888, true)
-        }
-
-
-
-
-
-
-    }
 
 
     @RequiresApi(Build.VERSION_CODES.M)
-    public fun checkandGetpermissions(){
-        if(checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+    public fun checkandGetpermissions() {
+        if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
             requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 100)
-        }
-        else{
+        } else {
             Toast.makeText(this, "Camera permission granted", Toast.LENGTH_SHORT).show()
         }
     }
--- app/src/main/java/com/example/burnproject/MyApi.kt
+++ app/src/main/java/com/example/burnproject/MyApi.kt
@@ -2,11 +2,16 @@ package com.example.burnproject
 
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface MyApi {
     @POST("upload")
     fun uploadImage(@Body request: ImageRequest): Call<ApiResponse>
+
+    @GET("endpoint")
+    fun getFloat(): Call<Float>
+
 }
 
 data class ImageRequest(val image: String)
